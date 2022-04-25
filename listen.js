772ab282d875c83a41016e61b571efffca0416b8
mezzDavies <93786981+mezzDavies@users.noreply.github.com>
2022-04-25 19:39:49 +0100

    proper syntax for PORT

--- listen.js
+++ listen.js
@@ -1,7 +1,5 @@
 const app = require("./app.js");
-const { port = 9090 } = process.env;
 
-app.listen(port, (err) => {
-  if (err) throw err;
-  console.log(`app listening on ${port}`);
-});
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT);
